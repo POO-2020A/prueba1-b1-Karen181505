5d4b93b50aa72f474ff265ce983c455f726bdcfb
Karen181505 <66264779+Karen181505@users.noreply.github.com>
2020-07-06 12:00:48 -0500

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -25,7 +25,52 @@ class Palindrome {
     System.out.print("In:");
     String s = inp.nextLine();
    	//write your code below
-   	
-   	
+     String cadena="";
+     String unionCadena1="";
+     String unionCadena2="";
+
+     // quitar los espacios de la cadena
+      String[] cadena1 = s.split(" ");
+
+     for(int i=0; i< cadena1.length; i++){
+       if(cadena1[i].substring(i, i+1).equals(cadena1[i].substring(i, i+1).toUpperCase())){
+         unionCadena1+=cadena1[i].substring(i, i+1).toLowerCase();
+       }else{
+         unionCadena1+=cadena1[i];
+       }
+     }
+
+   	for(int j=s.length()-1;j>= 0; j--){
+       if((s.substring(j, j+1).equals("á"))){
+         cadena+=s.replace("á", "a");
+       }else if(s.substring(j, j+1).equals("é")){
+         cadena+=s.replace("é", "e");
+       }else if(s.substring(j, j+1).equals("í")){
+         cadena+=s.replace("í", "i");
+       }else if(s.substring(j, j+1).equals("ó")){
+         cadena+=s.replace("ó", "o");
+       }else if(s.substring(j, j+1).equals("ú")){
+         cadena+=s.replace("ú", "u");
+       }else if(s.substring(j, j+1).equals(s.toUpperCase())){
+
+         cadena+=s.substring(j, j+1).toLowerCase();
+       }else{
+         cadena+=s.substring(j, j+1);
+       }
+     }
+     String[] cadena2 = cadena.split(" ");
+      
+     for(int i=0; i< cadena2.length; i++){
+       if(cadena2[i].substring(i, i+1).equals(cadena2[i].substring(i, i+1).toUpperCase())){
+         unionCadena2+=cadena2[i].substring(i, i+1).toLowerCase();
+       }else{
+         unionCadena2+=cadena2[i];
+       }
+     }
+     if(unionCadena2.equals(unionCadena1)){
+       System.out.println("true");
+     }else{
+       System.out.println("false");
+     }
   }
 }
